f21a8c242bafe5320a320115ba2359df3b9921d0
your name <namlinh2340@gmail.com>
2023-07-06 22:13:49 +0700

    nhan-vien add

--- src/main/java/com/example/datn/controller/NhanVienController.java
+++ src/main/java/com/example/datn/controller/NhanVienController.java
@@ -1,4 +1,13 @@
 package com.example.datn.controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/nhan-vien")
 public class NhanVienController {
+    @GetMapping("/hien-thi")
+    public String add(Model model){
+        return null;
+    }
 }
