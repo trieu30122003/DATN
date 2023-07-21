acaaf0bc8955d0c4e62d7df54ec05e6cc2542769
ACER <tranducdo2811@gmail.com>
2023-07-21 15:53:04 +0700

    update hoa don

--- src/main/java/com/example/datn/controller/HoaDonController.java
+++ src/main/java/com/example/datn/controller/HoaDonController.java
@@ -1,5 +1,55 @@
 package com.example.datn.controller;
 
+import com.example.datn.model.*;
+import com.example.datn.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000/")
+@Controller
+@RequestMapping("/hoa-don")
 public class HoaDonController {
+    @Autowired
+    HoaDonService hoaDonService;
+    @Autowired
+    KhachHangService khachHangService;
+    @Autowired
+    NhanVienService nhanVienService;
+    @Autowired
+    LichSuHoaDonService lichSuHoaDonService;
+    @Autowired
+    PhuongThucThanhToanService phuongThucThanhToanService;
+
+    @RequestMapping(value = "/hien-thi/", method = RequestMethod.GET)
+    public ResponseEntity<List<HoaDon>> listAll() {
+        List<HoaDon> list = hoaDonService.getAll();
+        if (list.isEmpty()) {
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        List<KhachHang> list1 = khachHangService.getAll();
+        if (list.isEmpty()) {
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        List<NhanVien> list2 =nhanVienService .getAll();
+        if (list.isEmpty()) {
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        List<LichSuHoaDon> list3= lichSuHoaDonService .getAll();
+        if (list.isEmpty()) {
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        List<PhuongThucThanhToan> list4= phuongThucThanhToanService .getAll();
+        if (list.isEmpty()) {
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<List<HoaDon>>(list, HttpStatus.OK);
+    }
 
 }
